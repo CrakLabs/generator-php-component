97a796244a337749939fd9fe4282d4ad34b082cd
bcolucci <bcolucci@crakmedia.com>
2014-11-26 13:23:35 -0500

    Initial commit

---
+++ app/phpcomp-1417026210665/src/SampleClass.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is property of crakmedia (http://crakmedia.com)
+ * Created by bcolucci <bcolucci@crakmedia.com>
+ * @copyright 2014 Crakmedia
+ */
+
+namespace Crak\Component\GeneratorPhpComponent;
+
+/**
+ * Class SampleClass
+ * @namespace Crak\Component\GeneratorPhpComponent
+ */
+class SampleClass {
+
+} 
---
+++ app/phpcomp-1417026210665/test/Unit/SampleUnitTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is property of crakmedia (http://crakmedia.com)
+ * Created by bcolucci <bcolucci@crakmedia.com>
+ * @copyright 2014 Crakmedia
+ */
+
+namespace Crak\Component\GeneratorPhpComponent\Test\Unit;
+
+/**
+ * Class SampleUnitTest
+ * @namespace Crak\Component\GeneratorPhpComponent\Test\Unit
+ */
+class SampleUnitTest extends \PHPUnit_Framework_TestCase {
+
+    public function testShouldDoSomething() {
+        $this->markTestSkipped('TODO');
+    }
+} 
